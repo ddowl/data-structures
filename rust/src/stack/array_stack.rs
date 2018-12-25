96cd0d6e93508f5a220c1e17e56fa7ecd5565cc9
Drew Dowling <drewdowling37@gmail.com>
2018-12-25 11:44:44 -0800

    Rust ArrayStack
    My first stab at using Boxes hold onto heap space. I'd like to figure
    out how to do this without using the `vec!` macro, and how to initialize
    heap space without initializing it to anything in particular. There's
    probably a better way to return Options from pop() and peek() as well.

--- rust/src/stack/array_stack.rs
+++ rust/src/stack/array_stack.rs
@@ -1,43 +1,71 @@
 use super::Stack;
 
+/**
+This stack is implemented with a dynamically resizing array that's manually managed.
+*/
 pub struct ArrayStack {
     capacity: usize,
     top: usize,
-    arr: Box<[isize]>,
+    arr: Box<[i32]>,
 
 }
 
 impl ArrayStack {
     fn new(capacity: usize) -> Self {
-
         ArrayStack {
             capacity: capacity,
             top: 0,
+            // initializing with vec might be cheating. I'm wasting time filling up the array with zeros. How would I allocate this manually?
             arr: vec![0; capacity].into_boxed_slice(),
         }
+    }
+
+    fn resize(&mut self, new_len: usize) {
+        // Wasting time filling up with zeros
+        let mut new_allocation = vec![0; new_len].into_boxed_slice();
+        new_allocation[..self.capacity].clone_from_slice(&self.arr);
+        println!("{:?}", new_allocation);
+        self.arr = new_allocation;
+        self.capacity = new_len;
     }
 }
 
 impl Stack for ArrayStack {
     fn is_empty(&self) -> bool {
-        unimplemented!()
+        self.size() == 0
     }
 
-    fn size(&self) -> u32 {
-        unimplemented!()
+    fn size(&self) -> usize {
+        self.top
     }
 
     fn push(&mut self, item: i32) {
-        unimplemented!()
+        if self.top == self.capacity {
+            self.resize(self.capacity * 2);
+        }
+        self.arr[self.top] = item;
+        self.top += 1;
     }
 
     fn pop(&mut self) -> Option<i32> {
-        unimplemented!()
+        if self.is_empty() {
+            None
+        } else {
+            self.top -= 1;
+            let item = self.arr[self.top];
+            Some(item)
+        }
     }
 
-    fn peek(&self) -> Option<i32> {
-        unimplemented!()
+    fn peek(&self) -> Option<&i32> {
+        if self.is_empty() {
+            None
+        } else {
+            Some(&self.arr[self.top - 1])
+        }
     }
+
+
 }
 
 #[cfg(test)]
@@ -92,6 +120,17 @@ mod tests {
         assert_eq!(stack.pop(), Some(1));
         assert_eq!(stack.pop(), None);
         assert_eq!(stack.pop(), None);
+    }
+
+    #[test]
+    fn peek() {
+        let mut stack = ArrayStack::new(10);
+        assert_eq!(stack.peek(), None);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        assert_eq!(stack.peek(), Some(&3));
+        assert_eq!(stack.peek(), Some(&3));
     }
 
     #[test]
